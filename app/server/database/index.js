65380d46c495e0811708c6a3faf5af8b3b096349
Jason Desiderio <jmdesiderio@gmail.com>
2016-11-20 23:48:13 -0500

    Knex test

--- app/server/database/index.js
+++ app/server/database/index.js
@@ -1,29 +1,14 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('jasondesiderio', '', '', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'jasondesiderio',
+    password: '',
+    database: 'jasondesiderio'
+  },
+  migrations: {
+    tableName: 'migrations'
   }
 })
 
-const User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  birthday: Sequelize.DATE,
-  firstName: Sequelize.STRING,
-  lastName: Sequelize.STRING
-})
-
-User.create({
-  username: 'janedoe',
-  birthday: new Date(1980, 6, 20),
-  firstName: 'Jane',
-  lastName: 'Doe'
-}).then((jane) => {
-  console.log(jane.get({
-    plain: true
-  }))
-})
+console.log(knex)
